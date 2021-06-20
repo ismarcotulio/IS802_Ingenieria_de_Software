88e61fe5176709243f33e346402be34ce58fc172
Marco Ruiz <martulioruiz@gmail.com>
2021-06-20 12:50:35 -0600

    tr2_forntend 1
    Se hizo una demostracion para comprobar el funcionamiento de router outlet

--- Training/1 Vistas Angular/vista-angular/src/app/app.module.ts
+++ Training/1 Vistas Angular/vista-angular/src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { VistaMarco1Component } from './vista-marco1/vista-marco1.component';
+import { VistaMarco2Component } from './vista-marco2/vista-marco2.component';
+import { HijoMarco1Component } from './hijo-marco1/hijo-marco1.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    VistaMarco1Component,
+    VistaMarco2Component,
+    HijoMarco1Component
   ],
   imports: [
     BrowserModule,

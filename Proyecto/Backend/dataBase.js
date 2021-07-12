aae1d1c3ef8fa4acfa67e065f6acea46a9cf1948
Luis Banegas <55450718+Shadow504@users.noreply.github.com>
2021-07-12 15:57:14 -0600

    Actualizacion Backend

--- Proyecto/Backend/dataBase.js
+++ Proyecto/Backend/dataBase.js
@@ -3,7 +3,7 @@ var mysql = require('mysql2');
 conexion = mysql.createConnection({ 
     host: 'localhost',
     user: 'root',
-    password: 'root123',
+    password: ' ',
     database: 'ecommerce'
 });
 

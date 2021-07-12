fa39732c8bc0df799586435a576170d6d4cef2f1
Jafethfer <brando.fernandez@unah.hn>
2021-07-12 15:27:06 -0600

    main.js

--- Proyecto/Backend/dataBase.js
+++ Proyecto/Backend/dataBase.js
@@ -3,7 +3,7 @@ var mysql = require('mysql2');
 conexion = mysql.createConnection({ 
     host: 'localhost',
     user: 'root',
-    password: 'root123',
+    password: 'jafethfer10',
     database: 'ecommerce'
 });
 

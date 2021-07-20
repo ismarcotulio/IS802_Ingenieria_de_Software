9003bf8b8b5dc46b7e50226d50e8f9a0074db8f1
Marco Ruiz <martulioruiz@gmail.com>
2021-07-20 13:50:42 -0600

    Actualizacion mostrar productos y  servicio de busqueda
    1. El servicio para mostrar productos por fines practicos ahora se ejecutara en las vistas (ejemplo home) y no directamente en el componente product-shelf.
    2. Para parar la informacion de los productos desde la vista (ejemplo: home) hacia un componente hijo (ejemplo: product-shelf) se utiliza la tecnica one-way-data-binding que hace uso del decorador @input.
    3. al dar clic en el boton buscar de la barra de buscqueda, se pasa la keyword hacia el objeto history.state, luego se recarga el componente utilizando navigate para que este posteriormente ejecute el servicio product-search-service y retorne los nuevos productos por palabra clave.
    4. actualmente los productos que se retornan provienen de un mock-backend.

--- Proyecto/Frontend/pr-frontend/src/app/home/view-home/view-home.component.ts
+++ Proyecto/Frontend/pr-frontend/src/app/home/view-home/view-home.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { ProductService } from 'src/app/core/services/product/product.service';
+import { Product } from 'src/app/core/models/product/product-model';
+import { ProductSearchService } from './../../core/services/product/product-search.service';
 
 @Component({
   selector: 'app-view-home',
@@ -6,10 +9,23 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./view-home.component.css']
 })
 export class ViewHomeComponent implements OnInit {
-
-  constructor() { }
+  products: Product[] = [];
+  constructor(
+    private productService:ProductService,
+    private productSearchService: ProductSearchService
+  ) { }
 
   ngOnInit(): void {
+    if(history.state.data){
+      this.products =this.productSearchService.getProductsByKeyword()
+
+    }else{
+      this.products = this.productService.getAllProducts();
+    }
+  }
+
+  updateProducts(products:Product[]){
+    this.products = products
   }
 
 }

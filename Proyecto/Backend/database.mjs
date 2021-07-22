12b87463c85e655454ce2cc81d594fb1b403a198
Jafethfer <brando.fernandez@unah.hn>
2021-07-22 16:52:10 -0600

    ruta filtro multiple

--- Proyecto/Backend/database.mjs
+++ Proyecto/Backend/database.mjs
@@ -122,6 +122,18 @@ class Database{
           })
         })
       }
+
+      getMultipleFilters(categoria,departamento){
+        return new Promise((resolve, reject)=>{
+          this.conexion.query(`CALL getMultipleFilters(?,?)`,[categoria,departamento], (error,results, fields)=>{
+            if(error){
+              reject(error)
+            }else{
+              resolve(results[0])
+            }
+          })
+        })
+      }
 }
 
 export { Database }
--- Proyecto/Backend/main.js
+++ Proyecto/Backend/main.js
@@ -51,6 +51,110 @@ app.post('/signUp', async (req, res) => {
     authController.signUp(req, res, app.get("llave"))
 })
 
+app.get('/categoria-departamento', (req,res)=>{
+    let categoria = 0
+    let departamento = 0
+    switch (req.departamento){
+        case 'FranciscoMorazan':    
+            departamento = 1
+            break;
+        case 'comayagua':           
+            departamento = 2
+            break;
+        case 'islas-de-la-bahia':
+            departamento = 3
+            break;
+        case 'santa-barbara':
+            departamento = 4
+            break;
+        case 'colon':
+            departamento = 5
+            break;
+        case 'olancho':
+            departamento = 6
+            break;
+        case 'yoro':
+            departamento = 7
+            break;
+        case 'gracias-a-dios':
+            departamento = 8
+            break;
+        case 'cortes':
+            departamento = 9
+            break;
+        case 'el-paraiso':
+            departamento = 10
+            break;
+        case 'copan':
+            departamento = 11
+            break;
+        case 'ocotepeque':
+            departamento = 12
+            break;
+        case 'lempira':
+            departamento = 13
+            break;
+        case 'intibuca':
+            departamento = 14
+            break;
+        case 'la-paz':
+            departamento = 15
+            break;
+        case 'valle':
+            departamento = 16
+            break;
+        case 'atlantida':
+            departamento = 17
+            break;
+        case 'choluteca':
+            departamento = 18
+            break;
+        default:
+            res.send('Categoria invalida')
+    }
+    switch(req.categoria){
+        case 'tecnologia':
+            categoria = 1
+            break;
+        case 'arte-artesania':
+            categoria = 2
+            break;
+        case 'hogar':
+            categoria = 3
+            break;
+        case 'industrial':
+            categoria = 4
+            break;
+        case 'automotriz':
+            categoria = 5
+            break;
+        case 'salud-belleza':
+            categoria = 6
+            break;
+        case 'viajes-equipaje':
+            categoria = 7
+            break;
+        case 'alimentos':
+            categoria = 8
+            break;
+        case 'jugueteria':
+            categoria = 9
+            break;
+        case 'mascotas':
+            categoria = 10
+            break;
+        case 'deportes':
+            categoria = 11
+            break;
+        default:
+            res.send('Categoria invalida')
+    }
+    database.getMultipleFilters(categoria,departamento)
+    .then(results=>{
+        res.send(results)
+    })
+})
+
 app.post('/verify',tokenController.middleVerifyToken, function(req,res){
     tokenController.verifyToken(req,res,app.get('llave'))
 })

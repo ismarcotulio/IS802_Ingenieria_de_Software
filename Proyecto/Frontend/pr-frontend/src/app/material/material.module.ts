1643ad635ca3ad48812c9e217c89ceaf85740020
Marco Ruiz <martulioruiz@gmail.com>
2021-08-06 21:11:02 -0600

    Actualizacion -> Correcion Modulo material

--- Proyecto/Frontend/pr-frontend/src/app/material/material.module.ts
+++ Proyecto/Frontend/pr-frontend/src/app/material/material.module.ts
@@ -43,7 +43,7 @@ import { MatPaginatorModule } from '@angular/material/paginator';
     MatMenuModule,
     LayoutModule,
     MatSidenavModule,
-    MatListModule
+    MatListModule,
     MatBadgeModule,
     MatTableModule,
     MatPaginatorModule
@@ -66,7 +66,7 @@ import { MatPaginatorModule } from '@angular/material/paginator';
     MatMenuModule,
     LayoutModule,
     MatSidenavModule,
-    MatListModule
+    MatListModule,
     MatBadgeModule,
     MatTableModule,
     MatPaginatorModule

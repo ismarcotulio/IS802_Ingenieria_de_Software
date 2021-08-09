85b5709acf716c6f81884adb731d842f68315b58
Dayriela1994 <yoselin.montes@unah.hn>
2021-08-09 16:17:24 -0600

    pr_sp3_db_Dayriela
    Actualizacion del procedimiento Delete_Suscription

--- Proyecto/DB/SCRIPTS_Dayriela/Oficiales/Delete_Suscription.sql
+++ Proyecto/DB/SCRIPTS_Dayriela/Oficiales/Delete_Suscription.sql
@@ -4,9 +4,8 @@ DELIMITER //
 
 CREATE PROCEDURE DeleteSuscription(IN IdUser_parameter INT, IN IdCategory_parameter INT)
 BEGIN
- DELETE FROM SUSCRIPTION WHERE SUSCRIPTION.Id_User_FK = IdUser_parameter AND SUSCRIPTION.IdCategory_FK= IdCategory;
+ DELETE FROM SUSCRIPTION WHERE SUSCRIPTION.Id_User_FK = IdUser_parameter AND SUSCRIPTION.Id_Category_FK= IdCategory_parameter;
 
 END //
 
 DELIMITER ;
-

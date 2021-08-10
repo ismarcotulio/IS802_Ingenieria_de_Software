ea6e62fc2fd10a10e603362941aa6209c810c1aa
Dayriela1994 <yoselin.montes@unah.hn>
2021-08-10 01:30:20 -0600

    pr_sp3_db_Dayriela
    Devuelve las categorias

---
+++ Proyecto/DB/SCRIPTS_Dayriela/Oficiales/Return_Category.sql
@@ -0,0 +1,17 @@
+USE Ecommerce ; 
+
+DROP PROCEDURE IF EXISTS Return_Category; 
+
+DELIMITER //
+CREATE PROCEDURE Return_Category(IN Num INT)
+BEGIN 
+ IF (Num = 0 ) THEN
+ SELECT *FROM CATEGORY;
+ 
+ ELSE 
+  SELECT CATEGORY.Id , CATEGORY.Name , CATEGORY.Description FROM CATEGORY Limit 0,6 
+  ;
+  END IF ;
+END //
+
+DELIMITER ;

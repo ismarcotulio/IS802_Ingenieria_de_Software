e43721169446d391dc4f54a12026f59372abb994
Marco Ruiz <martulioruiz@gmail.com>
2021-08-18 02:48:28 -0600

    Actualizacion getUserProducts

--- Proyecto/DB/SCRIPTS_Dayriela/Oficiales/getUserProduct.sql
+++ Proyecto/DB/SCRIPTS_Dayriela/Oficiales/getUserProduct.sql
@@ -1,8 +1,28 @@
-use ecommerce; 
+USE ecommerce; 
 
-Delimiter //
+DROP PROCEDURE IF EXISTS getUserProduct;
+
+DELIMITER //
 
 CREATE PROCEDURE  getUserProduct(IN userId INT)
 Begin
-	SELECT * from PRODUCT WHERE Id_User_FK=userId;
-End//
+	SELECT 
+		PRODUCT.Id AS productId, PRODUCT.Name AS productName, PRODUCT.Brand AS productBrand,
+		PRODUCT.Cost AS productCost, PRODUCT.Description AS productDescription,
+		PRODUCT.Image AS productImage, PRODUCT.Date_Product AS productDate,
+		CATEGORY.Id AS categoryId, CATEGORY.Name AS categoryName, CATEGORY.Description AS categoryDescription,
+		DEPARTMENT.Name AS departmentName, STATE.Name AS stateName, ACTIVITY_STATE.Name AS activityStateName	
+	FROM 
+		PRODUCT
+	JOIN
+		CATEGORY ON PRODUCT.Id_Category_FK = CATEGORY.Id
+	JOIN
+		DEPARTMENT ON PRODUCT.Id_Department_FK = DEPARTMENT.Id
+	JOIN
+		STATE ON PRODUCT.Id_State_FK = STATE.Id
+	JOIN
+		ACTIVITY_STATE ON PRODUCT.Id_State = ACTIVITY_STATE.Id_State
+	WHERE Id_User_FK=userId;
+END //
+
+DELIMITER ;

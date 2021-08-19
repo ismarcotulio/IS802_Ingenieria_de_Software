e3d1bd58d85cee3a258b618b49840c1c1a949348
Marco Ruiz <martulioruiz@gmail.com>
2021-08-18 21:41:39 -0600

    h19 Completada
    1.Graficos estadisticos en el modulo administrador

--- Proyecto/Frontend/pr-frontend/src/app/admin/components/statistics/statistics.component.ts
+++ Proyecto/Frontend/pr-frontend/src/app/admin/components/statistics/statistics.component.ts
@@ -9,36 +9,17 @@ import { StatisticsService } from '../../services/statistics/statistics.service'
   styleUrls: ['./statistics.component.css']
 })
 export class StatisticsComponent {
-  /** Based on the screen size, switch from standard to one column per row */
-  cards = this.breakpointObserver.observe(Breakpoints.Handset).pipe(
-    map(({ matches }) => {
-      if (matches) {
-        return [
-          { title: 'Card 1', cols: 1, rows: 1 },
-          { title: 'Card 2', cols: 1, rows: 1 },
-          { title: 'Card 3', cols: 1, rows: 1 },
-          { title: 'Card 4', cols: 1, rows: 1 }
-        ];
-      }
 
-      return [
-        { title: 'Card 1', cols: 2, rows: 1 },
-        { title: 'Card 2', cols: 1, rows: 1 },
-        { title: 'Card 3', cols: 1, rows: 2 },
-        { title: 'Card 4', cols: 1, rows: 1 }
-      ];
-    })
-  );
 
-  constructor(private breakpointObserver: BreakpointObserver, private statisticService:StatisticsService) {}
+  constructor(private statisticService:StatisticsService) {}
 
 
   optionTimeSelect(event:any){
     if(event.target.value != 0){
-      
+
       this.statisticService.setNewTimePost({time:event.target.value}).subscribe(result =>{
         // console.log(result);
-        
+
       })
     }
 
